e69532a30a5ec3af287788975733d824a59a45a3
PhilouLaudun <77538675+PhilouLaudun@users.noreply.github.com>
2024-01-30 19:46:12 +0100

    Commit apres finalisation interface page contact avant programmation envoi email

--- client/src/componants/autres/CarteEmail.js
+++ client/src/componants/autres/CarteEmail.js
@@ -3,7 +3,6 @@ import CancelTwoToneIcon from "@mui/icons-material/CancelTwoTone";
 import SendTwoToneIcon from "@mui/icons-material/SendTwoTone";
 import emailjs from "emailjs-com"; // Import de la librairie emailjs
 
-
 // composant CarteEmail  de la page structure (props passées: onCancel: pour fermer la boite de dialogue)
 const CarteEmail = (props) => {
   // style de icone de fermeture et d'envoi
@@ -16,14 +15,16 @@ const CarteEmail = (props) => {
       transform: "scale(1.4)", // Déplace l'icône vers le haut au survol
     },
   };
-  const onCancel = props.onCancel;// fonction de retour lors de la fermeture de la modale 
-  const [message, setMessage] = useState("");// message d'erreur
+  const onCancel = props.onCancel; // fonction de retour lors de la fermeture de la modale
+  const [name, setName] = useState(""); // variable contenant le nom de la personne envoyant le message
+  const [email, setEmail] = useState(""); // variable contenant l'email de la personne envoyant le message
+  const [message, setMessage] = useState(""); // message d'erreur
   const serviceId = "service_3b23au8"; // variable contenant le numéro d'identification pour emailjs
   const templateId = "template_19udd2q"; // variable contenant le numéro d'identification pour emailjs
   const publicKey = "user_wycCeh0xbzJrIlAiWMPQA"; // clé pour email.js
   const recipientEmail = "philippe.boudinaud@orange.fr"; // pour envoyer vers un autre destinataire il faut rajouter {{to_email}} dans le champ To EMail du template sinon cela laisse l'adresse par défaut. Cela implique que si on veut utiliser une adresse d'un partenaire, on charge recipientEmail avec cette adresse et on modifie le champ du template. Voir on crée un nouveau template
-const [sendStatus, setSendStatus] = useState(null);// variable définissant le status de l'envoi 
-  var messageavertissement = document.getElementById("messageavertissement");// adresse de la div d'affichage du message
+  const [sendStatus, setSendStatus] = useState(null); // variable définissant le status de l'envoi
+  var messageavertissement = document.getElementById("messageavertissement"); // adresse de la div d'affichage du message
   const sendMessage = () => {
     if (message === "") {
       messageavertissement.innerHTML = "le message doit être renseigné";
@@ -59,37 +60,41 @@ const [sendStatus, setSendStatus] = useState(null);// variable définissant le s
 
   return (
     <main className="carteEmail">
-      {/* zone du titre */}
-      <div className="titreCarteEmail">
-        <div>Message au prestataire</div>
-        {/* icone d'annulation */}
-        <CancelTwoToneIcon
-          className="cancel"
-          sx={iconeStyle}
-          onDoubleClick={onCancel}
-        />
-          {/* icone d'envoi d'un email : l'icone devient verte si success sinon rouge */}
-        <SendTwoToneIcon
-          className={`send ${sendStatus === "success" ? "success" : ""} ${
-            sendStatus === "error" ? "error" : ""
-          }`}
-          sx={iconeStyle}
-          onDoubleClick={sendMessage}
-        />
-      </div>
       {/* zone de saisie du message  */}
-      <div className="zoneMessage">
-        <textarea
-          id="message"
-          name="message"
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="message *"
-          value={message}
+      <input
+        type="text"
+        id="name"
+        name="name"
+        required
+        onChange={(e) => setName(e.target.value)}
+        placeholder="nom *"
+        value={name}
+      />
+      <div className="email-content">
+        <input
+          type="mail"
+          id="email"
+          name="email"
           required
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="email *"
+          value={email}
         />
-        {/* zone du message si celui est vide  */}
-        <div id="messageavertissement" className="messageparten"></div>
       </div>
+      <textarea
+        id="message"
+        name="message"
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="message *"
+        value={message}
+        required
+      />
+      <div className="zonebouton">
+        <button className="boutonoccase">Envoyer</button>
+      </div>
+
+      {/* zone du message si celui est vide  */}
+      <div id="messageavertissement" className="messageparten"></div>
     </main>
   );
 };
